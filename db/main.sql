1e95cae7a7dac654654debde058397c7fd800f6a
David Davis <xantus@xantus.org>
2009-08-29 20:12:10 -0700

    add sql file

---
+++ db/main.sql
@@ -0,0 +1,16 @@
+BEGIN TRANSACTION;
+CREATE TABLE session (
+        sid          VARCHAR(40) PRIMARY KEY,
+        data         TEXT,
+        expires      INTEGER UNSIGNED NOT NULL,
+        UNIQUE(sid)
+    );
+CREATE TABLE users (
+        user_id       VARCHAR(40) PRIMARY KEY,
+        email         VARCHAR(255) NOT NULL,
+        password      VARCHAR(255) NOT NULL,
+        UNIQUE(user_id)
+    );
+INSERT INTO users VALUES('test','test@cometdesktop.org','testing');
+COMMIT;
+
