ce511030cb5cb797779a55526395db1cacb6deaa
David Davis <xantus@xantus.org>
2010-01-31 12:48:04 -0800

    updated the startup files

---
+++ bin/lib/Bootstrapper.pm
@@ -0,0 +1,28 @@
+package Bootstrapper;
+
+use FindBin;
+
+BEGIN {
+    my $p = "$FindBin::Bin/..";
+    
+    print "plugins: ";
+
+    foreach my $d ( "$p/libs", "$p/libs/plugins" ) {
+        opendir( my $dh, $d );
+        my @dirs = grep {
+            !/^\./ &&
+            !/^plugins$/ &&
+            ( -d "$d/$_" || -l "$d/$_" )
+        } readdir( $dh );
+        closedir( $dh );
+
+        foreach ( sort { $a cmp $b } @dirs ) {
+            print "$_ ";
+            eval( "use lib \"$d/$_\";" );
+        }
+    }
+
+    print "\n";
+}
+
+1;
