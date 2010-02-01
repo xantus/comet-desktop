69fc38348d3f29ecb9a81b4d2bfea2738fb3d859
David Davis <xantus@xantus.org>
2010-01-31 19:51:57 -0800

    cleanup

--- bin/lib/Bootstrapper.pm
+++ bin/lib/Bootstrapper.pm
@@ -5,8 +5,6 @@ use FindBin;
 BEGIN {
     my $p = "$FindBin::Bin/..";
     
-    print "plugins: ";
-
     foreach my $d ( "$p/libs", "$p/libs/plugins" ) {
         opendir( my $dh, $d );
         my @dirs = grep {
@@ -17,13 +15,10 @@ BEGIN {
         closedir( $dh );
 
         foreach ( sort { $a cmp $b } @dirs ) {
-            print "$_ ";
             eval( "use lib \"$d/$_\";" );
             eval( "use lib \"$d/$_/lib\";" ) if ( -d "$d/$_/lib" )
         }
     }
-
-    print "\n";
 }
 
 1;
