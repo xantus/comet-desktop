7745c0173ebe50e86affc76e3c1363fdb93ea5b1
David Davis <xantus@xantus.org>
2010-03-02 23:37:20 -0800

    fix the bootstrapper for Win32 (strawberry-perl)

--- bin/lib/Bootstrapper.pm
+++ bin/lib/Bootstrapper.pm
@@ -19,18 +19,18 @@ BEGIN {
         my $path = catfile( $p, $d );
         opendir( my $dh, $path ) or die "could not opendir $path: $!";
         my @dirs = grep {
-            local $_ = catfile( $path, $_ );
+            my $t = catfile( $path, $_ );
             !/^\./ &&
             !/^plugins$/ &&
-            ( -d || -l )
+            ( -d $t || -l $t )
         } readdir( $dh ) or die "could not readdir $path: $!";
         closedir( $dh );
 
         foreach ( sort { $a cmp $b } @dirs ) {
             local $_ = catfile( $path, $_ );
             my $lib = catfile( $_, 'lib' );
-            eval( qq|use lib "$_";| );
-            eval( qq|use lib "$lib";| ) if ( -d $lib );
+            eval( qq|use lib '$_';| );
+            eval( qq|use lib '$lib';| ) if ( -d $lib );
             push( @configs, $_ ) if ( -e catfile( $_, 'config.json' ) );
         }
     }
