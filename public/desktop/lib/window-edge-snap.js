9864ef18fd3e61c7febaab3721b81bbc4312152a
David Davis <xantus@xantus.org>
2010-05-01 04:14:31 -0400

    half maximize a window by dragging it to the left or right side of the screen (like win7)  next: restore original size when the window is dragged

--- public/desktop/lib/window-edge-snap.js
+++ public/desktop/lib/window-edge-snap.js
@@ -1,6 +1,6 @@
 
 /* Window Edge Snapping for Extjs 
- * Version: 1.9
+ * Version: 2.0
  *
  * Copyright (c) 2008-2009 - David W Davis, All Rights Reserved
  *
@@ -10,7 +10,7 @@
  *
  * License: Same as Extjs 3.0 ( GPL / Commercial )
  * You may only use this commercially if you purchase
- * a commercial license from Extjs LLC
+ * a commercial license from Ext JS
  *
  * Please do not remove this header
  */
@@ -28,7 +28,7 @@ Ext.override( Ext.Window, {
 // but since Ext.Window does not pass a config obj to Ext.Window.DD
 // we'll just set them here
 Ext.ux.WindowSnap = {
-    version: '1.9',
+    version: '2.0',
     snapRange: 25, // px
     dragSnap: false,
     dropSnap: true,
@@ -37,8 +37,17 @@ Ext.ux.WindowSnap = {
 
 Ext.ux.WindowSnap.DD = Ext.extend( Ext.Window.DD, {
 
-    startDrag: function() {
-        Ext.ux.WindowSnap.DD.superclass.startDrag.apply( this, arguments );
+    startDrag: function( x, y ) {
+        /*
+        if ( this.win._prevPosition ) {
+            //this.win.setPagePosition( this.win._prevPosition.x, this.win._prevPosition.y );
+            this.win.setPagePosition( x, y );
+            this.win.setSize( this.win._prevPosition.width, this.win._prevPosition.height );
+            this.win.syncSize();
+            delete this.win._prevPosition;
+        }
+        */
+        Ext.ux.WindowSnap.DD.superclass.startDrag.call( this, x, y );
         if ( Ext.ux.WindowSnap.dragSnap )
             this._getSnapData();
     },
@@ -47,55 +56,154 @@ Ext.ux.WindowSnap.DD = Ext.extend( Ext.Window.DD, {
         Ext.ux.WindowSnap.DD.superclass.endDrag.apply( this, arguments );
         if ( Ext.ux.WindowSnap.dropSnap ) {
             this._getSnapData();
-            var pos = this.win.getPosition();
-            this.setSnapXY( this.win.el, pos[ 0 ], pos[ 1 ], true );
+            if ( this._halfmaxed ) {
+                var box = this._halfmaxed.getBox();
+                this._halfmaxed.hide();
+                this._halfmaxed.remove();
+                delete this._halfmaxed;
+                this.win._prevPosition = this.win.getBox();
+                this.win.setPagePosition( box.x, box.y );
+                this.win.setSize( box.width, box.height );
+            } else {
+                var pos = this.win.getPosition();
+                this.setSnapXY( this.win.el, pos[ 0 ], pos[ 1 ], undefined, undefined, true );
+            }
         }
         this.snapDD = [];
+        delete this._box;
+        delete this._halfmaxed;
     },
-
-    alignElWithMouse: function( el, iPageX, iPageY ) {
+    onDrag: function( e ) {
+        if ( !this._box )
+            this._getViewArea();
+        var iPageX = e.getPageX(), iPageY = e.getPageY();
         var oCoord = this.getTargetCoord( iPageX, iPageY );
-        var fly = el.dom ? el : Ext.fly( el, '_dd' );
+        var fly = this.proxy.dom ? this.proxy : Ext.fly( this.proxy, '_dd' );
+        var useXLeft = iPageX, useXRight = iPageX;
+        var x = null, y = null, width = null, height = null;
         if ( !this.deltaSetXY ) {
-            var aCoord = [ oCoord.x, oCoord.y ];
-            fly.setXY( aCoord );
-            var newLeft = fly.getLeft( true );
-            var newTop  = fly.getTop( true );
+            fly.setXY( [ oCoord.x, oCoord.y ] );
+            var newLeft = fly.getLeft( true ); // css pos
+            var newTop  = fly.getTop( true );  // css pos
             this.deltaSetXY = [ newLeft - oCoord.x, newTop - oCoord.y ];
         } else {
-            if ( Ext.ux.WindowSnap.dragSnap )
-                this.setSnapXY( fly, oCoord.x + this.deltaSetXY[ 0 ], oCoord.y + this.deltaSetXY[ 1 ] );
-            else
-                fly.setLeftTop( oCoord.x + this.deltaSetXY[ 0 ], oCoord.y + this.deltaSetXY[ 1 ] );
+            // left edge
+            if ( useXLeft < 1 || ( useXLeft < 1 && iPageY <= 0 ) ) {
+                if ( !this._halfmaxed ) {
+                    var ghost = this.win.createGhost();
+                    ghost.setLocation(0, this._box.y);
+                    var half = this._box.right / 2;
+                    if ( iPageY <= 0 ) {
+                        ghost._type = 'tl';
+                        ghost.setSize(half, this._box.bottom / 2);
+                    } else {
+                        ghost._type = 'l';
+                        ghost.setSize(half, this._box.bottom - this._box.y);
+                    }
+                    this._halfmaxed = ghost;
+                    animate = true;
+                }
+                /*
+                if ( !this._halfmaxed ) {
+                    var ghost = this.win.createGhost();
+                    ghost.setLocation(0, this._box.y);
+                    this._halfmaxed = ghost;
+                }
+                var s = this._halfmaxed.getSize();
+                var xy = this._halfmaxed.getXY();
+                var halfh = this._box.right / 2;
+                halfv = this._box.bottom / 2;
+                if ( iPageY <= 0 && s[ 1 ] != halfv ) {
+                    this._halfmaxed.setSize(halfh, halfv, true);
+                } else if ( iPageY >= this._box.bottom && s[ 1 ] != halfv || xy[ 1 ] != halfv ) {
+                    if ( xy[ 1 ] != halfv )
+                        this._halfmaxed.setLocation(0, halfv, true);
+                    this._halfmaxed.setSize(halfh, halfv, true);
+                } else if ( s[ 1 ] != this._box.bottom - this._box.y ) {
+                    this._halfmaxed.setSize(halfh, this._box.bottom - this._box.y, true);
+                }
+                */
+            }
+            // right edge
+            if ( useXRight + 1 >= this._box.right ) {
+                if ( !this._halfmaxed ) {
+                    var ghost = this.win.createGhost();
+                    ghost.setLocation(this._box.right / 2, this._box.y);
+                    this._halfmaxed = ghost;
+                    if ( iPageY <= 0 ) {
+                        ghost._type = 'tr';
+                        ghost.setSize(half, this._box.bottom / 2);
+                    } else {
+                        ghost._type = 'r';
+                        ghost.setSize(half, this._box.bottom - this._box.y);
+                    }
+                }
+                var s = this._halfmaxed.getSize();
+                var halfh = this._box.right / 2;
+                halfv = this._box.bottom / 2;
+                if ( iPageY <= 0 && s[ 1 ] != halfv ) {
+                    this._halfmaxed.setSize(halfh, halfv, true);
+                } else if ( s[ 1 ] != this._box.bottom - this._box.y ) {
+                    this._halfmaxed.setSize(halfh, this._box.bottom - this._box.y, true);
+                }
+            }
+/*
+            if ( this._halfmaxed ) {
+                if ( useXLeft < 10 ) {
+                    x = 0;
+                    y = this._box.y;
+                } else if ( this._box.right - useXRight < 10 ) {
+                    x = this._box.right / 2;
+                    y = this._box.y;
+                }
+            }
+*/
+            // restore
+            if ( this._halfmaxed && useXLeft > 10 && this._box.right - useXRight > 10 ) {
+                if ( this._halfmaxed ) {
+                    this._halfmaxed.hide();
+                    this._halfmaxed.remove();
+                    delete this._halfmaxed;
+                }
+            }
+            if ( x === null )
+                x = oCoord.x + this.deltaSetXY[ 0 ];
+            if ( y === null )
+                y = oCoord.y + this.deltaSetXY[ 1 ];
+            
+            if ( Ext.ux.WindowSnap.dragSnap ) {
+                this.setSnapXY( fly, x, y, width, height );
+            } else {
+                if ( width !== null ) {
+                    fly.setBounds( x, y, width, height );
+                } else {
+                    fly.setLeftTop( x, y );
+                }
+            }
         }
 
-        this.cachePosition( oCoord.x, oCoord.y );
-        this.autoScroll( oCoord.x, oCoord.y, el.offsetHeight, el.offsetWidth );
+        //this.cachePosition( oCoord.x, oCoord.y );
+        this.cachePosition( x, y );
+        this.autoScroll( x, y, fly.dom.offsetHeight, fly.dom.offsetWidth );
         return oCoord;
     },
 
-    setSnapXY: function( fly, x, y, drop ) {
+    setSnapXY: function( fly, x, y, width, height, drop ) {
         var ox = x, oy = y;
         var lx = [], ly = [];
         var box = this.win.getBox();
         var range = Ext.ux.WindowSnap.snapRange;
-        // get the window's parent container
-        var el = Ext.fly( this.win.el.parent() );
-        var viewTop = el.getTop();
-        var viewLeft = el.getLeft();
-        var viewBottom = el.getBottom();
-        var viewRight = el.getRight();
         // check the edges of the window's parent
         // right and left
-        if ( Math.abs( x - viewLeft ) < range )
-            lx.push( viewLeft );
-        else if ( Math.abs( ( x + box.width ) - viewRight ) < range )
-            lx.push( viewRight - box.width );
+        if ( Math.abs( x - this._box.x ) < range )
+            lx.push( this._box.x );
+        else if ( Math.abs( ( x + box.width ) - this._box.right ) < range )
+            lx.push( this._box.right - box.width );
         // top and bottom
-        if ( Math.abs( y - viewTop ) < range )
-            ly.push( viewTop );
-        else if ( Math.abs( ( y + box.height ) - viewBottom ) < range )
-            ly.push( viewBottom - box.height );
+        if ( Math.abs( y - this._box.y ) < range )
+            ly.push( this._box.y );
+        else if ( Math.abs( ( y + box.height ) - this._box.bottom ) < range )
+            ly.push( this._box.bottom - box.height );
         
         // now check all visible windows
         for ( var i = 0, len = this.snapDD.length; i < len; i++ ) {
@@ -175,7 +283,16 @@ Ext.ux.WindowSnap.DD = Ext.extend( Ext.Window.DD, {
             fly.setXY( [ x, y ] );
     },
 
+    _getViewArea: function() {
+        // get the window's parent container
+        var el = Ext.fly( this.win.el.parent() );
+        this._box = el.getBox();
+        this._box.right = this._box.x + this._box.width;
+    },
+
     _getSnapData: function() {
+        if ( !this._box )
+            this._getViewArea();
         var snapDD = this.snapDD = [];
         var win = this.win;
         win.manager.each(function( w ) {
