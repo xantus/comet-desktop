694a6f1eedbb33c103c3d9f8bf48553c4d57a313
David Davis <xantus@xantus.org>
2010-05-17 23:28:41 -0400

    apps can now have a routes.conf file, app routes are forced under desktop/apps/[app_name]

--- libs/comet-desktop/CometDesktop.pm
+++ libs/comet-desktop/CometDesktop.pm
@@ -47,6 +47,7 @@ sub startup {
     $self->_load_config;
     $self->_add_hooks;
     $self->_set_routes;
+    $self->_configure_apps;
 
     return;
 }
@@ -113,8 +114,6 @@ sub _load_config {
         stash_key => 'config'
     });
 
-    my $static = $self->plugin( static_fallback => $config );
-
     foreach( @{$config->{mojo_plugins}} ) {
         s/-/_/g;
         next if ( $_ eq 'static_fallback' );
@@ -165,6 +164,12 @@ sub _load_config {
     $self->session->default_expiration( $config->{mojo_session_default_expiration} )
         if ( defined $config->{mojo_session_default_expiration} );
 
+    return;
+}
+
+sub _configure_apps {
+    my $self = shift;
+
     my $db = DBIx::Simple->connect( @{$config}{qw( db_interface db_user db_pass )} )
         or die DBIx::Simple->error;
 
@@ -172,22 +177,70 @@ sub _load_config {
 
     $db->disconnect;
 
+    my $static = $self->plugin( static_fallback => $config );
+
     foreach( @{$config->{cometdesktop_apps}} ) {
         warn "configuring app $_->{app_id} : $_->{app_name} : $_->{app_desc}\n";
-        my $dir = $self->home->rel_dir( File::Spec->catfile( 'apps', $_->{app_name}, 'public' ) );
-        if ( -d $dir ) {
-            warn "adding static fallback for apps/$_->{app_name}/ to $dir\n";
-            $static->add( "/desktop/apps/$_->{app_name}/" => $dir );
+        my $dir = $self->home->rel_dir( File::Spec->catfile( 'apps', $_->{app_name} ) );
+        my $public = File::Spec->catfile( $dir, 'public' );
+        my $routes = File::Spec->catfile( $dir, 'routes.conf' );
+        if ( -d $public ) {
+            warn "adding static fallback for apps/$_->{app_name}/ to $public\n";
+            $static->add( "/desktop/apps/$_->{app_name}/" => $public );
         }
 #        if ( -e File::Spec->catfile( $dir, 'plugin.conf' ) ) {
 #            my $plug = $_->{app_name}; $plug =~ s/-/_/g;
 #            $self->plugin( $plug, $config );
 #        }
+        $self->_set_app_routes( $_, $routes ) if ( -e $routes );
     }
 
     if ( $self->mode eq 'development' ) {
         require Data::Dumper;
         warn Data::Dumper->Dump([$config],['config']);
+    }
+
+    return;
+}
+
+sub _set_app_routes {
+    my ( $self, $app_info, $file ) = @_;
+
+    $app_info->{app_path} = "/desktop/apps/$app_info->{app_name}/";
+
+    warn "route for $app_info->{app_path}\n";
+
+#    my $base = $self->routes->route( $app_info->{app_path} );
+    my $base = $self->routes;
+
+    my $cfg = $self->_json_config( $file, { %$app_info, routes => $base } );
+    $cfg = [$cfg] if defined $cfg && ref $cfg eq 'HASH';
+
+    $app_info->{routes} = $cfg;
+
+    if ( $self->mode eq 'development' ) {
+        warn Data::Dumper->Dump([$cfg],[$app_info->{app_name}.'_routes']);
+    }
+
+    return unless defined $cfg && ref $cfg eq 'ARRAY';
+
+    foreach ( @$cfg ) {
+        $_->{route} ||= '';
+        $_->{route} =~ s/^\///g if $_->{route} =~ m/^\//;
+        my $r = $base->route( $app_info->{app_path}.$_->{route} );
+        $r->parse( $_->{parse} ) if $_->{parse};
+        $r->pattern( $_->{pattern} ) if $_->{pattern};
+        $r->conditions( $_->{conditions} ) if $_->{conditions};
+        $r->over( $_->{over} ) if $_->{over};
+        $r->to( $_->{to} ) if $_->{to};
+        my $defaults ||= {};
+        $defaults->{controller} = $_->{controller} if $_->{controller};
+        $defaults->{action} = $_->{action} if $_->{action};
+        $r->to( $defaults ) if keys %$defaults;
+        $r->via( $_->{via} ) if $_->{via};
+        $r->name( $_->{name} ) if $_->{name};
+        $r->inline( 1 ) if $_->{inline};
+        $r->websocket( 1 ) if $_->{websocket};
     }
 
     return;
@@ -202,6 +255,41 @@ sub _set_routes {
     $self->routes->route( '/desktop/logout' )->via( 'post' )->to( 'desktop#logout' );
 
     return;
+}
+
+sub _json_config {
+    my ( $self, $file, $template ) = @_;
+
+    # Slurp UTF-8 file
+    open FILE, "<:encoding(UTF-8)", $file
+      or die qq/Couldn't open config file "$file": $!/;
+    my $encoded = do { local $/; <FILE> };
+    close FILE;
+
+    # Instance
+    my $prepend = 'my $app = shift;';
+
+    # Be less strict
+    $prepend .= q/no strict 'refs'; no warnings 'redefine';/;
+
+    # Helper
+    $prepend .= "sub app; *app = sub { \$app };";
+
+    # Be strict again
+    $prepend .= q/use strict; use warnings;/;
+
+    # Render
+    my $mt = Mojo::Template->new($template || {});
+    $mt->prepend($prepend);
+    $encoded = $mt->render($encoded, $self);
+
+    # Parse
+    my $json   = Mojo::JSON->new;
+    my $config = $json->decode($encoded);
+    my $error  = $json->error;
+    die qq/Couldn't parse config file "$file": $error/ if !$config && $error;
+
+    return $config;
 }
 
 1;
